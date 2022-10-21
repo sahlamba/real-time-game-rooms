f95f36c99959d8b7864318e0fe62f5ab6c849f8a
Sahil Lamba <sahil.lamba95@gmail.com>
2022-10-21 18:27:53 +0530

    Simplify Promise usage in apiClient

--- client/src/utils/apiClient.js
+++ client/src/utils/apiClient.js
@@ -5,60 +5,42 @@ const headers = {
   'Content-Type': 'application/json',
 }
 
-export const createGame = (player, settings) => {
-  return new Promise(async (resolve, reject) => {
-    try {
-      const res = await fetch(`${API_BASE_URL}/api/game`, {
-        method: 'POST',
-        headers,
-        body: JSON.stringify({ player, settings }),
-      })
-      const { ok, game, message } = await res.json()
-      if (!ok) {
-        reject(message)
-      }
-      resolve(game)
-    } catch (error) {
-      reject(error)
-    }
+export const createGame = async (player, settings) => {
+  const res = await fetch(`${API_BASE_URL}/api/game`, {
+    method: 'POST',
+    headers,
+    body: JSON.stringify({ player, settings }),
   })
+  const { ok, game, message } = await res.json()
+  if (!ok) {
+    throw new Error(message)
+  }
+  return game
 }
 
-export const getGameById = (gameCode) => {
-  return new Promise(async (resolve, reject) => {
-    try {
-      const res = await fetch(`${API_BASE_URL}/api/game?code=${gameCode}`, {
-        method: 'GET',
-        headers,
-      })
-      const { ok, game, message } = await res.json()
-      if (!ok) {
-        reject(message)
-      }
-      resolve(game)
-    } catch (error) {
-      reject(error)
-    }
+export const getGameById = async (gameCode) => {
+  const res = await fetch(`${API_BASE_URL}/api/game?code=${gameCode}`, {
+    method: 'GET',
+    headers,
   })
+  const { ok, game, message } = await res.json()
+  if (!ok) {
+    throw new Error(message)
+  }
+  return game
 }
 
-export const getPlayerGamesById = (playerId) => {
-  return new Promise(async (resolve, reject) => {
-    try {
-      const res = await fetch(
-        `${API_BASE_URL}/api/game/player/games?playerId=${playerId}`,
-        {
-          method: 'GET',
-          headers,
-        },
-      )
-      const { ok, playerGameCodeMappings, message } = await res.json()
-      if (!ok) {
-        reject(message)
-      }
-      resolve(playerGameCodeMappings)
-    } catch (error) {
-      reject(error)
-    }
-  })
+export const getPlayerGamesById = async (playerId) => {
+  const res = await fetch(
+    `${API_BASE_URL}/api/game/player/games?playerId=${playerId}`,
+    {
+      method: 'GET',
+      headers,
+    },
+  )
+  const { ok, playerGameCodeMappings, message } = await res.json()
+  if (!ok) {
+    throw new Error(message)
+  }
+  return playerGameCodeMappings
 }
